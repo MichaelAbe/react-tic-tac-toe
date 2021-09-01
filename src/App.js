e9427206c1c8b253abe7c42ad51d47376e7200c4
MichaelAbe <michael.k.abe@gmail.com>
2021-09-01 14:22:28 -0400

    added board and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '')
   return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
+    <div className="board"></div>
   );
 }
 

467678a47ab0dbed0c1dc9d0ffe274c47d9aa0f4
MichaelAbe <michael.k.abe@gmail.com>
2021-09-09 12:16:14 -0400

    compiling issue

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
     <div className="board">
       <div className="row">
         <Square val={board[0]} chooseSquare={() => {alert(0)}} />
-        {/* <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
-        <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
-        <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
-        <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
-        <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
-        <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
+        <Square val={board[1]} chooseSquare={() => {alert(1)}}/>
+        {/* <Square val={board[2]} chooseSquare={() => {alert(2)}}/>
+        <Square val={board[3]} chooseSquare={() => {alert(3)}}/>
+        <Square val={board[4]} chooseSquare={() => {alert(4)}}/>
+        <Square val={board[5]} chooseSquare={() => {alert(5)}}/>
+        <Square val={board[6]} chooseSquare={() => {alert(6)}}/>
         <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
         <Square val={board[0]} chooseSquare={() => {alert(0)}}/>
      */}
@@ -25,8 +25,8 @@ function App() {
       <div className="row"></div>
       <div className="row"></div>
     </div>
-    </div>
-  );
+    </div1
+  )
 }
 
 export default App;

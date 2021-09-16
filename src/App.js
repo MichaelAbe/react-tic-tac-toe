e4911ce4d99bcfe4ee99afb643994bd787670771
MichaelAbe <michael.k.abe@gmail.com>
2021-09-16 14:26:39 -0400

    working out square location

--- src/App.js
+++ src/App.js
@@ -13,17 +13,20 @@ function App() {
       <div className="row">
         <Square val={board[0]} chooseSquare={() => {alert(0)}} />
         <Square val={board[1]} chooseSquare={() => {alert(1)}}/>
-        <Square val={board[2]} chooseSquare={() => {alert(2)}}/>
+        <Square val={board[2]} chooseSquare={() => {alert(2)}}/>     
+      </div>
+      <div className="row">
         <Square val={board[3]} chooseSquare={() => {alert(3)}}/>
         <Square val={board[4]} chooseSquare={() => {alert(4)}}/>
         <Square val={board[5]} chooseSquare={() => {alert(5)}}/>
+      </div>
         <Square val={board[6]} chooseSquare={() => {alert(6)}}/>
         <Square val={board[7]} chooseSquare={() => {alert(7)}}/>
         <Square val={board[8]} chooseSquare={() => {alert(8)}}/>
-    
+      <div className="row">
+
       </div>
-      <div className="row"></div>
-      <div className="row"></div>
+    
     </div>
     </div>
   );
